6474b0cb52ec758e215710cb9abd436ba32f53b8
pumchanpom <pum.chanpom@gmail.com>
2023-03-27 04:13:17 +0700

    Add files and make changes.

---
+++ shiftCipher.js
@@ -0,0 +1,63 @@
+// const cipher = new ShiftCipher(2);
+// cipher.encrypt('I love to code!'); // returns 'K NQXG VQ EQFG!'
+// // cipher.decrypt('K <3 OA RWRRA'); // returns 'i <3 my puppy'
+
+// Feel free to reference the Unicode Table as well as the JavaScript String methods including: 
+// toUpperCase(), toLowerCase(), charCodeAt() and fromCharCode() 
+
+
+class ShiftCipher {
+    constructor(shift) {
+        this.shift = shift;
+    }
+    
+    get getShift() {
+        return this.shift;
+    }
+
+    encrypt(text) {
+        let textUpper = text.toUpperCase();      
+        let textArr = [];
+        for (let index in textUpper) { 
+            let letter = textUpper[index];
+            if (/[A-Z]/.test(letter)) {
+                let charCode = textUpper.charCodeAt(index) + this.shift;
+                if (charCode >= 65 && charCode <= 90) {
+                    textArr.push(String.fromCharCode(charCode));
+                } else {
+                    textArr.push(String.fromCharCode(charCode-26));
+                }
+            } else {
+                textArr.push(letter);
+            }
+        }
+        console.log(textArr.join(''));
+    }
+
+    decrypt(text) {
+        let textLower = text.toLowerCase();      
+        let textArr = [];
+        for (let index in textLower) { 
+            let letter = textLower[index];
+            if (/[a-z]/.test(letter)) {
+                let charCode = textLower.charCodeAt(index) - this.shift;
+                if (charCode >= 97 && charCode <= 122) {
+                    textArr.push(String.fromCharCode(charCode));
+                } else {
+                    textArr.push(String.fromCharCode(charCode+26));
+                }
+
+            } else {
+                textArr.push(letter);
+            }
+        }
+        console.log(textArr.join(''));
+    }
+}
+
+const cipherOneLetter = new ShiftCipher(1);
+cipherOneLetter.encrypt('Z'); // returns 'A'
+
+const cipher = new ShiftCipher(2);
+cipher.encrypt('I love to code!'); // returns 'K NQXG VQ EQFG!'
+cipher.decrypt('K <3 OA RWRRA'); // returns 'i <3 my puppy'
